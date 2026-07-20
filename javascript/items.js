dd246ab1a98b19e057e95cc47df3c6e1bfe097ff
Aaditya432 <aaditya8183@gmail.com>
2026-07-20 18:14:32 +0530

    Update items.js

--- javascript/items.js
+++ javascript/items.js
@@ -57,7 +57,7 @@ console.log(data)
 function itemsFruits(){
 itemBag = namit.map( item => {
   for(let i = 0; i < products.length; i++){
-   if(item == products[i].id){
+   if(item != products[i].id){
     return products[i];
     }
 }
