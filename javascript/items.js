f50ee64aff069067735c88c0f4e7de197685eb51
Aaditya432 <aaditya8183@gmail.com>
2026-07-21 16:49:55 +0530

    Update items.js

--- javascript/items.js
+++ javascript/items.js
@@ -187,6 +187,7 @@ details.innerHTML+= `
    
 // })
    function displayItems(){
+     itembag.innerHTML = ' ';
      itemBag.forEach(items =>{
    itemsContainer23.innerHTML  += `<section class="items4">
                 <div class="images3">
