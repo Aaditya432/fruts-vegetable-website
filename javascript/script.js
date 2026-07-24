8d3f8305bfef021a35e62d66b08c52775cf13658
Aaditya432 <aaditya8183@gmail.com>
2026-07-24 15:36:38 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -99,12 +99,12 @@ let name = document.getElementById("name").value;
       emailid: email,
       createdAt: new Date()
     });
-
-    alert("Signup success!");
-    window.location.href = "/fruts-vegetable-website/files/vegetables.html";
-    name = '';
+     name = '';
     email = '';
     password = '';
+    alert("Signup success!");
+    window.location.href = "/fruts-vegetable-website/files/vegetables.html";
+   
   } catch (error) {
     alert("Signup Error: " + error.message);
   }
