9bc77628602108ced5573dfe7f9b2edf5b9ed458
Aaditya432 <aaditya8183@gmail.com>
2026-07-28 15:19:28 +0530

    Update firebaseconfig.js

--- javascript/firebaseconfig.js
+++ javascript/firebaseconfig.js
@@ -2,7 +2,7 @@
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-mport { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
  
 const firebaseConfig = {
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = getApps().length === 0? initializeApp(firebaseConfig) : getApps()[0];
 // 4. Turant baad App Check init - YAHI IMPORTANT HAI
 const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('TUMHARI_SITE_KEY_YAHA'), // 6Lc... wali key
+  provider: new ReCaptchaV3Provider('6LcVU18tAAAAAOAhGM1-oML_w-luo1DyzHhOBkjE'), // 6Lc... wali key
   isTokenAutoRefreshEnabled: true 
 });
 
